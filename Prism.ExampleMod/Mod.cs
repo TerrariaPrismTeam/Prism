8f3b920293634750c51c17a36a9aa74d97f6f571
Meowmaritus <derp250@gmail.com>
2015-07-19 22:12:34 -0500

    Added assembly icons, did some work on the example mod, and found various issues

--- Prism.ExampleMod/Mod.cs
+++ Prism.ExampleMod/Mod.cs
@@ -12,12 +12,16 @@ namespace Prism.ExampleMod
     public class Mod : ModDef
     {
         bool hasPizza = false;
+        bool hasAnt = false;
+        bool hasPizzant = false;
+        bool hasPizzantzioli = false;
 
         protected override Dictionary<string, ItemDef> GetItemDefs()
         {
             return new Dictionary<string, ItemDef>
             {
-                { "Pizza", new ItemDef("Pizza", getTex: () => GetResource<Texture2D>("Pizza.png"),
+                { "Pizza", new ItemDef("Pizza", getTex: () => GetResource<Texture2D>("Resources\\Textures\\Items\\Pizza.png"),
+                    descr: new ItemDescription("LOTZA SPA-pizza. It's pizza.", "'MMmmmmmmm'", false, true),
                     useTime: 15,
                     useAnimation: 15,
                     consumable: true,
@@ -25,10 +29,65 @@ namespace Prism.ExampleMod
                     rare: ItemRarity.Lime,
                     useSound: 2,
                     useStyle: ItemUseStyle.Eat,
-                    holdStyle: ItemHoldStyle.BreathingReed,
+                    holdStyle: ItemHoldStyle.HeldLightSource,
                     healLife: 400,
                     value: new ItemValue(50, 10, 2),
-                    buff: new ItemBuff(BuffID.WellFed, 60 * 60 * 30)) }
+                    buff: new ItemBuff(BuffID.WellFed, 60 * 60 * 30)
+                    ) },
+                { "Ant", new ItemDef("Ant", getTex: () => GetResource<Texture2D>("Resources\\Textures\\Items\\Ant.png"),
+                    descr: new ItemDescription("By ants, for ants.", "'B-but...ants aren't this big!'", false, true),
+                    damageType: ItemDamageType.Melee,
+                    autoReuse: true,
+                    useTime: 12,
+                    useAnimation: 20,
+                    maxStack: 1,
+                    rare: ItemRarity.LightPurple,
+                    useSound: 1,
+                    damage: 60,
+                    knockback: 4f,
+                    width: 30,
+                    height: 30,
+                    useStyle: ItemUseStyle.Stab,
+                    holdStyle: ItemHoldStyle.Default,
+                    value: new ItemValue(0, 40, 8, 25),
+                    scale: 1.1f
+                    ) },
+                { "Pizzant", new ItemDef("Pizzant", getTex: () => GetResource<Texture2D>("Resources\\Textures\\Items\\Pizzant.png"),
+                    descr: new ItemDescription("The chaotic forces of italian spices and insects and bread.", "", false, true),
+                    damageType: ItemDamageType.Melee,
+                    autoReuse: true,
+                    useTime: 15,
+                    useAnimation: 20,
+                    maxStack: 1,
+                    rare: ItemRarity.Yellow,
+                    useSound: 1,
+                    damage: 80,
+                    knockback: 4f,
+                    width: 30,
+                    height: 30,
+                    useStyle: ItemUseStyle.Stab,
+                    holdStyle: ItemHoldStyle.Default,
+                    value: new ItemValue(1, 34, 1, 67),                    
+                    scale: 1.1f
+                    ) },
+                { "Pizzantzioli", new ItemDef("Pizzantzioli", getTex: () => GetResource<Texture2D>("Resources\\Textures\\Items\\Pizzantzioli.png"),
+                    descr: new ItemDescription("The forces of ants and pizza come together as one.", "The name is Italian for 'KICKING ASS'! YEAH!", false, true),
+                    damageType: ItemDamageType.Melee,
+                    autoReuse: true,
+                    useTime: 20,
+                    useAnimation: 16,
+                    maxStack: 1,
+                    rare: ItemRarity.Cyan,
+                    useSound: 1,
+                    damage: 150,
+                    knockback: 10f,
+                    width: 30,
+                    height: 30,
+                    useStyle: ItemUseStyle.Swing,
+                    holdStyle: ItemHoldStyle.Default,
+                    value: new ItemValue(2, 51, 3, 9),
+                    scale: 1.1f
+                    ) }
             };
         }
 
@@ -46,6 +105,27 @@ namespace Prism.ExampleMod
                         {
                             inv[i].SetDefaults(ItemDef.ByName["Pizza", Info.InternalName].Type);
                             hasPizza = true;
+                            inv[i].stack = inv[i].maxStack;
+                            continue;
+                        }
+                        else if (!hasAnt)
+                        {
+                            inv[i].SetDefaults(ItemDef.ByName["Ant", Info.InternalName].Type);
+                            hasAnt = true;
+                            inv[i].stack = inv[i].maxStack;
+                            continue;
+                        }
+                        else if (!hasPizzant)
+                        {
+                            inv[i].SetDefaults(ItemDef.ByName["Pizzant", Info.InternalName].Type);
+                            hasPizzant = true;
+                            inv[i].stack = inv[i].maxStack;
+                            continue;
+                        }
+                        else if (!hasPizzantzioli)
+                        {
+                            inv[i].SetDefaults(ItemDef.ByName["Pizzantzioli", Info.InternalName].Type);
+                            hasPizzantzioli = true;
                             inv[i].stack = inv[i].maxStack;
                             continue;
                         }
