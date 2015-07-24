0ddc6bda19bc2ce9b00dbe0152e36ec935b84481
PoroCYon <pietersluys2@gmail.com>
2015-07-24 14:41:20 +0200

    Updated Newtonsoft.Json to 7.0.0.0 instead of changing the version in the assembly ref

--- Prism.Injector/Patcher/TerrariaPatcher.cs
+++ Prism.Injector/Patcher/TerrariaPatcher.cs
@@ -9,9 +9,9 @@ namespace Prism.Injector.Patcher
 {
     public static class TerrariaPatcher
     {
-        readonly static Version
-            V7_0_0_0 = new Version(7, 0, 0, 0),
-            V4_5_0_0 = new Version(4, 5, 0, 0);
+        //readonly static Version
+        //    V7_0_0_0 = new Version(7, 0, 0, 0),
+        //    V4_5_0_0 = new Version(4, 5, 0, 0);
 
         internal static CecilContext   c;
         internal static MemberResolver r;
@@ -51,12 +51,12 @@ namespace Prism.Injector.Patcher
 
             c.PrimaryAssembly.CustomAttributes.Add(new CustomAttribute(ivt_ctor, Encoding.UTF8.GetBytes("Prism")));
         }
-        static void FixNewtonsoftJsonReferenceVersion()
-        {
-            foreach (var ar in c.PrimaryAssembly.MainModule.AssemblyReferences)
-                if (ar.Name == "Newtonsoft.Json" && ar.Version == V7_0_0_0)
-                    ar.Version = V4_5_0_0;
-        }
+        //static void FixNewtonsoftJsonReferenceVersion()
+        //{
+        //    foreach (var ar in c.PrimaryAssembly.MainModule.AssemblyReferences)
+        //        if (ar.Name == "Newtonsoft.Json" && ar.Version == V7_0_0_0)
+        //            ar.Version = V4_5_0_0;
+        //}
 
         public static void Patch(CecilContext context, string outputPath)
         {
@@ -68,7 +68,7 @@ namespace Prism.Injector.Patcher
 
             Publicify();
             //AddInternalsVisibleToAttr();
-            FixNewtonsoftJsonReferenceVersion();
+            //FixNewtonsoftJsonReferenceVersion();
 
             ItemPatcher.Patch();
             NpcPatcher .Patch();
