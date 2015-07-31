6520741c413cc76fa796957b329c8998a89cee8a
Will Huxtable <will@willhuxtable.com>
2015-07-31 21:00:05 +0100

    Add OS X support to Prism.sh

--- Prism.sh
+++ Prism.sh
@@ -2,6 +2,10 @@
 
 command -v mono >/dev/null 2>&1 || { echo >&2 "You must install Mono to use Prism. See the documentation."; exit 1;  }
 
-LD_LIBRARY_PATH=$(pwd)/lib64:$LD_LIBRARY_PATH
+if [ "$(uname)" == "Darwin" ]; then
+	LD_LIBRARY_PATH=$(pwd)/osx:$LD_LIBRARY_PATH
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+	LD_LIBRARY_PATH=$(pwd)/lib64:$LD_LIBRARY_PATH
+fi
 
 mono Prism.exe
