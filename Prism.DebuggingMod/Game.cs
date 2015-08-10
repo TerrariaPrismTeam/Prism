9aa96665875b9b3b4ce9c355bee3b24cc885fd66
Meowmaritus <derp250@gmail.com>
2015-08-10 14:02:53 -0500

    Added blank template for Prism.DebuggingMod

---
+++ Prism.DebuggingMod/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Prism.API.Behaviours;
+using Terraria;
+
+namespace Prism.DebuggingMod
+{
+    sealed class Game : GameBehaviour
+    {
+        public override void PostUpdate()
+        {
+            
+		}
+    }
+}
---
+++ Prism.DebuggingMod/Mod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LitJson;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Prism.API;
+using Prism.API.Defs;
+using Terraria;
+using Terraria.ID;
+
+namespace Prism.DebuggingMod
+{
+    public class Mod : ModDef
+    {
+        protected override ContentHandler CreateContentHandler()
+        {
+            return new Content();
+        }
+    }
+}
