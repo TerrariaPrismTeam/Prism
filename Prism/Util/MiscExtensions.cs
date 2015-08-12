add23f47cbf7c25abdb31e98fe99a653ce83cb16
PoroCYon <pietersluys2@gmail.com>
2015-08-12 21:28:24 +0200

    Added monaic bind functions for Lazy, KeyValuePair and Tuple to MiscExtensions

--- Prism/Util/MiscExtensions.cs
+++ Prism/Util/MiscExtensions.cs
@@ -44,5 +44,21 @@ namespace Prism.Util
         {
             return m.HasValue ? map(m.Value) : default(TOut);
         }
+        public static Lazy<TOut> Bind<TIn, TOut>(this Lazy<TIn> m, Func<TIn, TOut> map)
+        {
+            return new Lazy<TOut>(() => map(m.Value));
+        }
+        public static KeyValuePair<TOutKey, TOutValue> Bind<TInKey, TOutKey, TInValue, TOutValue>(this KeyValuePair<TInKey, TInValue> m, Func<TInKey, TOutKey> mapK, Func<TInValue, TOutValue> mapV)
+        {
+            return new KeyValuePair<TOutKey, TOutValue>(mapK(m.Key), mapV(m.Value));
+        }
+        public static Tuple<TOut1, TOut2> Bind<TIn1, TOut1, TIn2, TOut2>(this Tuple<TIn1, TIn2> m, Func<TIn1, TOut1> map1, Func<TIn2, TOut2> map2)
+        {
+            return m == null ? null : Tuple.Create(map1(m.Item1), map2(m.Item2));
+        }
+        public static Tuple<TOut1, TOut2, TOut3> Bind<TIn1, TOut1, TIn2, TOut2, TIn3, TOut3>(this Tuple<TIn1, TIn2, TIn3> m, Func<TIn1, TOut1> map1, Func<TIn2, TOut2> map2, Func<TIn3, TOut3> map3)
+        {
+            return m == null ? null : Tuple.Create(map1(m.Item1), map2(m.Item2), map3(m.Item3));
+        }
     }
 }
