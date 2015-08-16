23673f1abdddf6fb308034d0516c6f5ea018b516
PoroCYon <pietersluys2@gmail.com>
2015-08-17 00:52:38 +0200

    I can't believe how borked dmcs is

--- Prism/Properties/AssemblyInfo.cs
+++ Prism/Properties/AssemblyInfo.cs
@@ -2,9 +2,11 @@
 using System.Reflection;
 using Prism;
 
+#if !UNIX
 [assembly: AssemblyTitle("Prism")]
 [assembly: AssemblyDescription("Modding API for Terraria 1.3 and beyond")]
 [assembly: AssemblyInformationalVersion(AssemblyInfo.NICE_VERSION)]
+#endif
 
 static partial class AssemblyInfo
 {
