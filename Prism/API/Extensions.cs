59f64bfa6bd4e6f4939f0edfd091d07e095bbe12
PoroCYon <pietersluys2@gmail.com>
2015-08-24 20:25:58 +0200

    Added unimplemented GetBehaviour extensions

--- Prism/API/Extensions.cs
+++ Prism/API/Extensions.cs
@@ -9,9 +9,8 @@ namespace Prism.API
 {
     public static class Extensions
     {
-        static TWanted GetBehaviour<TBehaviour, TEntity, TWanted>(EntityBHandler<TBehaviour, TEntity> handler)
-            where TEntity : class
-            where TBehaviour : EntityBehaviour<TEntity>
+        static TWanted GetBehaviour<TBehaviour, TWanted>(IOBHandler<TBehaviour> handler)
+            where TBehaviour : IOBehaviour
             where TWanted : TBehaviour
         {
             if (handler == null)
@@ -20,25 +19,52 @@ namespace Prism.API
             return (TWanted)handler.behaviours.FirstOrDefault(b => b is TWanted);
         }
 
+        public static TBehaviour GetBuffBehaviour<TBehaviour>(this Player p, int index)
+            where TBehaviour : BuffBehaviour
+        {
+            if (p.P_BuffBHandler != null)
+                return GetBehaviour<BuffBehaviour, TBehaviour>(p.P_BuffBHandler[index] as BuffBHandler);
+
+            return null;
+        }
+        public static TBehaviour GetBuffBehaviour<TBehaviour>(this NPC    n, int index)
+            where TBehaviour : BuffBehaviour
+        {
+            if (n.P_BuffBHandler != null)
+                return GetBehaviour<BuffBehaviour, TBehaviour>(n.P_BuffBHandler[index] as BuffBHandler);
+
+            return null;
+        }
+
         public static TBehaviour GetBehaviour<TBehaviour>(this Item       i )
             where TBehaviour : ItemBehaviour
         {
-            return GetBehaviour<ItemBehaviour, Item, TBehaviour>(i.P_BHandler as ItemBHandler);
+            return GetBehaviour<ItemBehaviour, TBehaviour>(i.P_BHandler as ItemBHandler);
+        }
+        public static TBehaviour GetBehaviour<TBehaviour>(this Mount      m )
+            where TBehaviour : MountBehaviour
+        {
+            var bh = m.P_BHandler as MountBHandler;
+
+            if (bh == null)
+                return null;
+
+            return (TBehaviour)bh.behaviours.FirstOrDefault(b => b is TBehaviour);
         }
         public static TBehaviour GetBehaviour<TBehaviour>(this NPC        n )
             where TBehaviour : NpcBehaviour
         {
-            return GetBehaviour<NpcBehaviour, NPC, TBehaviour>(n.P_BHandler as NpcBHandler);
+            return GetBehaviour<NpcBehaviour, TBehaviour>(n.P_BHandler as NpcBHandler);
         }
         public static TBehaviour GetBehaviour<TBehaviour>(this Projectile pr)
             where TBehaviour : ProjectileBehaviour
         {
-            return GetBehaviour<ProjectileBehaviour, Projectile, TBehaviour>(pr.P_BHandler as ProjBHandler);
+            return GetBehaviour<ProjectileBehaviour, TBehaviour>(pr.P_BHandler as ProjBHandler);
         }
         public static TBehaviour GetBehaviour<TBehaviour>(this Player     p )
             where TBehaviour : PlayerBehaviour
         {
-            return GetBehaviour<PlayerBehaviour, Player, TBehaviour>(p.P_BHandler as PlayerBHandler);
+            return GetBehaviour<PlayerBehaviour, TBehaviour>(p.P_BHandler as PlayerBHandler);
         }
     }
 }
