3666295329339021418c57878d26a5b492915071
PoroCYon <pietersluys2@gmail.com>
2015-08-28 22:04:41 +0200

    Made the patcher (non-msbuild-invocation) work on multiple platforms, some other enhancements, merged patcher file is now created

--- Patcher/Prism.TerrariaPatcher/Program.cs
+++ Patcher/Prism.TerrariaPatcher/Program.cs
@@ -18,88 +18,183 @@ namespace Prism.TerrariaPatcher
 {
     static class Program
     {
-        public static bool VsBuild = false;
+        public static bool MsBuild = false;
 
         public static string TerrariaExecutable = "Terraria.exe";
         public static string PrismAssembly = "Prism.Terraria.dll";
 
+        public readonly static bool IsWindows = Environment.OSVersion.Platform <= PlatformID.WinCE;
+
+        public readonly static string
+            TerrFoundMessage = "A 'Terraria.exe' file was found in the same folder as this patcher. Would you like to patch it now?" +
+                "If you choose not to patch that particular 'Terraria.exe' file you browse for the correct file.";
+
+        [STAThread]
         static int Main(string[] args)
         {
-            if (!ParseRuntimeArgs(args))
-                return 1;
-
-            Environment.CurrentDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-
-            if (!File.Exists(TerrariaExecutable))
-            {
-                Console.WriteLine("Terraria.exe not found. (full path: \"" + TerrariaExecutable + "\")");
-                // the file is added in the .gitignore (and so is the patched file)
-                if (VsBuild)
-                    Console.WriteLine("In order to build Prism, you must place a copy of your own Terraria.exe file in the '.\\References' directory.");
-
-                return 1;
-            }
-
-            var c = new CecilContext(TerrariaExecutable);
-
-            var d = Path.GetDirectoryName(PrismAssembly);
-            if (!Directory.Exists(d))
-                Directory.CreateDirectory(d);
-
-            // this will stop the build process if the patcher fails, because a reference in Prism.csproj will be missing
-            if (File.Exists(PrismAssembly))
-                File.Delete(PrismAssembly);
+            var toRem = new List<string>();
 
             try
             {
-                Patcher.Patch(c, PrismAssembly);
+                if (!ParseRuntimeArgs(args))
+                    return 1;
+
+                if (MsBuild)
+                    Environment.CurrentDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+                if (!File.Exists(TerrariaExecutable))
+                {
+                    Console.WriteLine("Terraria.exe not found. (full path: \"" + Path.GetFullPath(TerrariaExecutable) + "\")");
+                    // the file is added in the .gitignore (and so is the patched file)
+                    if (MsBuild)
+                        Console.WriteLine("In order to build Prism, you must place a copy of your own Terraria.exe file in the '."
+                            + Path.DirectorySeparatorChar /* be cross-platform */ + "References' directory.");
+
+                    return 1;
+                }
+
+                var d = Path.GetDirectoryName(TerrariaExecutable);
+
+                // just copy the files so the assembly resolving works, they'll be removed when it finished
+                if (!MsBuild)
+                {
+                    var  fs = new[] { "Newtonsoft.Json", "Steamworks.NET", "Ionic.Zip.CF" }.Select(n => Path.Combine(d, n + ".dll"));
+                    var ufs = new[] { "WindowsBase"    , "FNA"                            }.Select(n => Path.Combine(d, n + ".dll"));
+
+                    foreach (var s in fs.Concat(IsWindows ? new string[0] : ufs))
+                    {
+                        var t = Path.Combine(Environment.CurrentDirectory, Path.GetFileName(s));
+
+                        if (File.Exists(s) && !File.Exists(t) /* don't do a useless copy (and worse, remove it afterwards, even when it could be needed later) */)
+                        {
+                            File.Copy(s, t);
+                            toRem.Add(t);
+                        }
+                    }
+                }
+
+                var c = new CecilContext(TerrariaExecutable);
+
+                d = Path.GetDirectoryName(PrismAssembly);
+                if (!Directory.Exists(d))
+                    Directory.CreateDirectory(d);
+
+                // this will stop the build process if the patcher fails, because a reference in Prism.csproj will be missing
+                if (MsBuild && File.Exists(PrismAssembly))
+                    File.Delete(PrismAssembly);
+
+                try
+                {
+                    Console.Write("Patching, please wait... ");
+
+                    Patcher.Patch(c, PrismAssembly);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Something went wrong while patching " + Path.GetFileName(TerrariaExecutable) + ":");
+                    Console.WriteLine(e);
+
+                    return 1;
+                }
+
+                Console.WriteLine("Patching finished.");
+
+                return 0;
             }
-            catch (Exception e)
+            finally
             {
-                Console.WriteLine("Something went wrong while patching " + Path.GetFileName(TerrariaExecutable) + ".");
-                Console.WriteLine(e);
+                if (!MsBuild)
+                {
+                    Console.WriteLine("Press any key to exit...");
+                    Console.ReadKey(true);
+                }
 
-                return 1;
+                foreach (var s in toRem)
+                    File.Delete(s);
             }
-
-            return 0;
         }
 
         public static bool ParseRuntimeArgs(string[] unparsedArgs)
         {
             string[] args = unparsedArgs.Select(x => x.Trim()).ToArray();
-            if (args[0].ToLowerInvariant() == "vsbuild")
-                VsBuild = true;
-            else if (args.Length >= 1)
+            if (args.Length > 0)
             {
-                TerrariaExecutable = args[0];
+                int off = 0;
 
-                if (args.Length >= 2)
-                    PrismAssembly = args[1];
+                if (args[0].ToUpperInvariant() == "MSBUILD")
+                {
+                    MsBuild = true;
+                    off++;
+                }
+
+                TerrariaExecutable = args[0 + off];
+
+                if (args.Length >= 2 + off)
+                    PrismAssembly = args[1 + off];
+                else
+                    PrismAssembly = Path.Combine(Path.GetDirectoryName(args[0 + off]), "Prism.Terraria.dll");
             }
             else
             {
                 if (File.Exists(TerrariaExecutable))
+                    if (IsWindows)
+                    {
+                        var result = MessageBox.Show(TerrFoundMessage, "Terraria.exe detected.", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+
+                        if (result == DialogResult.Yes)
+                            return true;
+                        if (result == DialogResult.Cancel)
+                            return false; //It's a Yes/No/Cancel box not a fucking Yes/No box quit fucking breaking things randomly
+                    }
+                    else
+                    {
+                        Console.Write(TerrFoundMessage + " (y/anything else): ");
+
+                        if (Console.ReadKey().Key == ConsoleKey.Y)
+                            return true;
+                    }
+
+                if (IsWindows)
                 {
-                    var result = MessageBox.Show("A 'Terraria.exe' file was found in the same folder as this patcher. Would you like to patch it now? If you choose not to patch that particular 'Terraria.exe' file you browse for the correct file.", "Terraria.exe detected.", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
-                    if (result == DialogResult.Yes)
-                        return true;
-                    if (result == DialogResult.Cancel)
-                        return false; //It's a Yes/No/Cancel box not a fucking Yes/No box quit fucking breaking things randomly
+                    var fod = new OpenFileDialog()
+                    {
+                        CheckFileExists = true,
+                        FileName = "Terraria.exe",
+                        Title = "Select your Terraria.exe file",
+                        AddExtension = true,
+                        DefaultExt = ".exe",
+                        Filter = "Terraria|Terraria.exe" // only match the string "Terraria.exe" (case-insensitive, because it's Windows)
+                    };
+
+                    var fodResult = fod.ShowDialog();
+                    if (fodResult == DialogResult.Cancel)
+                        return false;
+
+                    TerrariaExecutable = fod.FileName;
+                    PrismAssembly = Path.Combine(Path.GetDirectoryName(TerrariaExecutable), "Prism.Terraria.dll");
                 }
-
-                var fod = new OpenFileDialog()
+                else
                 {
-                    CheckFileExists = true,
-                    FileName = "Terraria.exe",
-                    Title = "Select your Terraria.exe file"
-                };
+                TRY:
+                    Console.Write("Please drop your Terraria.exe file on this window and press enter: ");
 
-                var fodResult = fod.ShowDialog();
-                if (fodResult == DialogResult.Cancel)
-                    return false;
+                    var s = Console.ReadLine();
 
-                TerrariaExecutable = fod.FileName;
+                    if (String.IsNullOrEmpty(s) || !File.Exists(s))
+                    {
+                        Console.Write("Invalid file. Try again? (y/anything else): ");
+
+                        if (Console.ReadKey().Key == ConsoleKey.Y)
+                            goto TRY;
+                        else
+                            return false;
+                    }
+                    else
+                    {
+                        TerrariaExecutable = Console.ReadLine();
+                        PrismAssembly = Path.Combine(Path.GetDirectoryName(args[0]), "Prism.Terraria.dll");
+                    }
+                }
             }
 
             return true;
