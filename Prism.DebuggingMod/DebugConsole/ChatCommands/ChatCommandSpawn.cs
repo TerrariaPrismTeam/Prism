8b395e265136fdcadb4d96f7120a21187b4da7f8
Meowmaritus <derp250@gmail.com>
2016-06-04 18:10:21 -0500

    WIP DebuggingMod stuff

---
+++ Prism.DebuggingMod/DebugConsole/ChatCommands/ChatCommandSpawn.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prism.DebuggingMod.ChatConsole.ChatCommands
+{
+    public class ChatCommandSpawn : ChatCommand
+    {
+        public ChatCommandSpawn()
+            : base(name          : ""
+                  ,description   : ""  
+                  ,usageText     : ""  
+                  ,requiresArgs  : true
+                  ,caseSensitive : true
+                  ,minArgs       : 1   
+                  ,maxArgs       : 1    
+                  )
+        {
+
+        }
+
+        public override void Run(string args, List<string> splitArgs)
+        {
+            
+        }
+
+        //public static void ItemGivenText(Queue<Item> itemsGiven)
+        //{
+        //    string text = "";
+
+        //    while (itemsGiven.Count > 0)
+        //    {
+        //        int i = itemsGiven.Dequeue();
+        //        text += " [i/s" + Main.item[i].stack + "/p" + Main.item[i].prefix + ":" + Main.item[i].type + "]";
+        //    }
+            
+        //    Helpers.Main.RandColorText("You have been given" + text + ".", true);
+        //}
+    }
+}
