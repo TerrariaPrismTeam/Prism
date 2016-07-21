acc45dac18e0d4f3a6f2710d240891e05b8ed49f
PoroCYon <porocyon@openmailbox.org>
2016-07-21 22:58:26 +0000

    update to 1.3.2.1

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -20,14 +20,14 @@ static partial class AssemblyInfo
 {
     public const string GUID = "fa53dc38-9b2b-45d3-818e-3e60f69143f6";
     public const string VERSION = "1.0.0.0";
-    public const string MIN_TERRARIA_VERSION = "1.3.2.0";
-    public const string MAX_TERRARIA_VERSION = "1.3.2.0";
+    public const string MIN_TERRARIA_VERSION  = "1.3.2.0";
+    public const string MAX_TERRARIA_VERSION  = "1.3.2.1";
     public const string DISP_TERRARIA_VERSION = "1.3.2";
 
     internal const string DEV_BUILD_VERSION = "0.0.0.0";
 }
 static partial class GitInfo
 {
-    public const string REPO = "Prism";
+    public const string REPO   = "Prism";
     public const string BRANCH = "develop";
 }
